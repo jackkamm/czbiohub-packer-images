a068bf516cc16f5db503450cf837ef0cceebc503
James Webber <jamestwebber@users.noreply.github.com>
2017-11-20 15:22:46 -0800

    refactored the sleep/dpkg code into its own script (#11)
    * refactored the sleep/dpkg code into its own script

---
+++ scripts/sleep_until_dpkg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "sleeping 30 sec"; sleep 30
+for try in {1..100}; do if [[ $try -gt 1 ]]; then echo "sleeping 10 sec"; sleep 10; fi; sudo dpkg --configure -a || continue; break; done
+sudo apt-get -f install
+yes | sudo apt-get install make
+yes | sudo apt-get install g++
+exit 0
