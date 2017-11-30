1035c049001ab7a78f50a0e15bb980e588bdc7c3
James Webber <jamestwebber@users.noreply.github.com>
2017-11-29 23:06:03 -0800

    new script for base ubuntu (#21)

--- scripts/sleep_until_dpkg.sh
+++ scripts/sleep_until_dpkg.sh
@@ -2,6 +2,10 @@
 echo "sleeping 30 sec"; sleep 30
 for try in {1..100}; do if [[ $try -gt 1 ]]; then echo "sleeping 10 sec"; sleep 10; fi; sudo dpkg --configure -a || continue; break; done
 sudo apt-get -f install
+sudo apt-get update
+sudo apt-get upgrade -y
 yes | sudo apt-get install make
 yes | sudo apt-get install g++
+yes | sudo apt-get install awscli
+
 exit 0
