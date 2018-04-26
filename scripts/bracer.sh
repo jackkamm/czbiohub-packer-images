81d90b8a63f406d7a112e938504e650e18f507c1
olgabot <olga.botvinnik@gmail.com>
2018-04-25 20:58:12 -0700

    update R installation

--- scripts/bracer.sh
+++ scripts/bracer.sh
@@ -13,6 +13,10 @@ conda install --yes -c bioconda bowtie2 trinity igblast blast kallisto graphviz 
 # Conda install requirements
 conda install -c bioconda --yes python=3.6 biopython matplotlib networkx=1.11 cycler numpy pandas pyparsing pytz scipy seaborn six mock future  cutadapt
 
+# Install R and Immcantation through R
+conda install --yes r-ggplot2
+echo 'install.packages(c("alakazam", "Rscript", "shazam", "tigger"))' |  R --vanilla
+
 cd /mnt/data
 git clone https://github.com/Teichlab/bracer
 cd bracer
@@ -20,14 +24,14 @@ pip install .
 
 
 cd /mnt/data
-aws s3 sync s3://olgabot-genomes genome
+aws s3 cp s3://olgabot-genomes genome
 
 cd /mnt/data/genome/hg38/gencode/v28/
 gunzip *.gz
 cd /mnt/data/genome/mm10/gencode/m17/
 gunzip *.gz
 
-cat "#Configuration file for BraCeR#
+echo "#Configuration file for BraCeR#
 
 [tool_locations]
 #paths to tools used by BraCeR for alignment, quantitation, etc
@@ -57,4 +61,7 @@ Mmus = /mnt/data/genome/mm10/gencode/m17/gencode.vM17.transcripts.fa
 #Path to where BraCeR was originally installed
 bracer_path = /mnt/data/bracer" > ~/.bracerrc
 
-bracer test -p 16 -c ~/.bracerrc
+bracer --help
+
+# bracer test -p 16 -c ~/.bracerrc
+
