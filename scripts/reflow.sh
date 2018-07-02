637c5c871642298aeff1926feffab89f0781723b
olgabot <olga.botvinnik@gmail.com>
2018-07-02 14:57:38 -0700

    Don't create s3 repository, use existing one

--- scripts/reflow.sh
+++ scripts/reflow.sh
@@ -58,7 +58,7 @@ sudo cp /tmp/system.conf /etc/systemd/system.conf
 
 # Send reflow setup commands to bashrc so they get set up for every user's AWS credentials
 echo "AWS_SDK_LOAD_CONFIG=1 reflow setup-ec2" >> ~/.bashrc
-echo "AWS_SDK_LOAD_CONFIG=1 reflow setup-s3-repository czbiohub-reflow-quickstart-cache" >> ~/.bashrc
+echo 'repository: s3,czbiohub-reflow-quickstart-cache' >> ~/.reflow/config.yaml
 echo "AWS_SDK_LOAD_CONFIG=1 reflow setup-dynamodb-assoc czbiohub-reflow-quickstart" >> ~/.bashrc
 
 source ~/.bashrc
