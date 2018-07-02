b3aef2f7fb06f5c62a9d6c4e4ee272515507f7f7
olgabot <olga.botvinnik@gmail.com>
2018-07-02 15:08:26 -0700

    Put concatenation command into bashrc

--- scripts/reflow.sh
+++ scripts/reflow.sh
@@ -58,8 +58,8 @@ sudo cp /tmp/system.conf /etc/systemd/system.conf
 
 # Send reflow setup commands to bashrc so they get set up for every user's AWS credentials
 echo "AWS_SDK_LOAD_CONFIG=1 reflow setup-ec2" >> ~/.bashrc
-echo 'repository: s3,czbiohub-reflow-quickstart-cache' >> ~/.reflow/config.yaml
 echo "AWS_SDK_LOAD_CONFIG=1 reflow setup-dynamodb-assoc czbiohub-reflow-quickstart" >> ~/.bashrc
+echo "echo 'repository: s3,czbiohub-reflow-quickstart-cache' >> ~/.reflow/config.yaml" >> ~/.bashrc
 
 source ~/.bashrc
 
