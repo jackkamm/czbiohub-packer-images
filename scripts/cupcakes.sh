ea660804a8ce87a9e17c130e7bbfcd6004586e71
olgabot <olga.botvinnik@gmail.com>
2018-10-15 17:34:17 -0700

    Add cupcakes & codingc

---
+++ scripts/cupcakes.sh
@@ -0,0 +1,14 @@
+export PATH=$HOME/anaconda/bin:$PATH # add to PATH
+echo 'export PATH=$HOME/anaconda/bin:$PATH'
+
+git clone https://github.com/czbiohub/cupcakes
+conda env update --name root --file cupcakes/2018/olgas_bioinformagician_tricks/environment.yml
+
+conda list
+
+conda info -a
+
+
+## Show available environments upon login
+echo "conda env list" >> ~/.bashrc
+source ~/.bashrc
