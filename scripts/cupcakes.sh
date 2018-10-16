5322dd4d6aeef9df5661931ee199ff2f7eab75c9
olgabot <olga.botvinnik@gmail.com>
2018-10-16 10:52:01 -0700

    fix branch name' git push '

--- scripts/cupcakes.sh
+++ scripts/cupcakes.sh
@@ -1,7 +1,7 @@
 export PATH=$HOME/anaconda/bin:$PATH # add to PATH
 echo 'export PATH=$HOME/anaconda/bin:$PATH'
 
-git clone https://github.com/czbiohub/cupcakes.git@olgabot/bioinformagician-part2-hotfix
+git clone -b olgabot/bioinformagician-part2-hotfix https://github.com/czbiohub/cupcakes.git
 conda env update --name root --file cupcakes/2018/olgas_bioinformagician_tricks/environment.yml
 
 conda list
